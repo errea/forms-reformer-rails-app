00f3036308eae49be8e306a005360d32200b66c7
errea <erreakezy@gmail.com>
2021-04-16 22:00:59 +0100

    Review-fix: remove commented code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,28 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
-  # GET /users or /users.json
+  
   def index
     @users = User.all
   end
 
-  # GET /users/1 or /users/1.json
+
   def show
     @user = User.find(params[:id])
   end
 
-  # GET /users/new
+  
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
+
   def edit
     @user = User.find(params[:id])
   end
 
-  # POST /users or /users.json
+
   def create
-    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
     if @user.save
       redirect_to root_path
@@ -32,13 +31,12 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
+  
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash.notice = "User '#{@user.username}' edited!"
       redirect_to @user
-      # redirect_to root_path(@user)
     else
       render :edit
     end
@@ -46,12 +44,10 @@ class UsersController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
